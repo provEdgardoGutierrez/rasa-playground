c2043082e4980f19748d2c096de8cb1548483b2a
Elijah Gutierrez <edgardo.gutierrez@providence.org>
2022-07-08 16:13:06 -0700

    fixed typo

--- .github/workflows/code_freeze.py
+++ .github/workflows/code_freeze.py
@@ -8,7 +8,7 @@ ENVIRONMENT = "stage"
 project_key = "smart-assistant"
 feature_flag_key = "code-freeze"
 url = "https://app.launchdarkly.com/api/v2/flags/" + project_key + "/" + feature_flag_key
-headers = {"Authorization": sys.argv[1:]}
+headers = {"Authorization": str(sys.argv[1:])}
 
 response = requests.get(url, headers=headers)
 
