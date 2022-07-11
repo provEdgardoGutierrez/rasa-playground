d857e9ae8b10dd92657a159c6bfd70619bab91f8
Elijah Gutierrez <edgardo.gutierrez@providence.org>
2022-07-11 09:13:10 -0700

    Added env consideration

--- .github/workflows/code_freeze.py
+++ .github/workflows/code_freeze.py
@@ -1,18 +1,28 @@
-
-
 import requests
 import os
 
-ENVIRONMENT = "stage"
+LD_API_KEY = os.getenv("LD_API_KEY")
+if not LD_API_KEY:
+    raise Exception(
+        f"The 'LD_API_KEY' environment variable has not been setup, has expired or was revoked in GH Secrets."
+    )
 
-project_key = "smart-assistant"
-feature_flag_key = "code-freeze"
-url = "https://app.launchdarkly.com/api/v2/flags/" + project_key + "/" + feature_flag_key
-LD_API_KEY = os.getenv('LD_API_KEY')
+ENVIRONMENT = os.getenv("BRANCH")
+if not ENVIRONMENT:
+    raise Exception(f"The 'ENVIRONMENT' environment variable was not set.")
+
+ACCEPTED_ENVIRONMENTS = ["dev", "stage"]
+if not ENVIRONMENT in ACCEPTED_ENVIRONMENTS:
+    raise Exception(
+        f"The 'ENVIRONMENT' environment variable is not one of the allowed values. Accepted: {ACCEPTED_ENVIRONMENTS}"
+    )
+
+URL = "https://app.launchdarkly.com/api/v2/flags/smart-assistant/code-freeze"
 headers = {"Authorization": LD_API_KEY}
-
-response = requests.get(url, headers=headers)
+response = requests.get(URL, headers=headers)
 
 data = response.json()
 if data["environments"][ENVIRONMENT]["on"]:
-    raise Exception(f"There is a code freeze in {ENVIRONMENT}. Merging new code is currently not permitted.")
+    raise Exception(
+        f"There is a code freeze in {ENVIRONMENT}. Merging new code is currently not permitted."
+    )
